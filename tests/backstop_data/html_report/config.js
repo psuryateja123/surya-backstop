e7336be78d57d6dcfd0e5e5e04dcde0a968f4836
surya teja <surya.teja24@yahoo.com>
2019-01-21 12:08:40 +0000

    initial commit
    initial commit

---
+++ tests/backstop_data/html_report/config.js
@@ -0,0 +1,98 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/nhsuk-frontend_A-Z_navigation_0_document_0_iPhone_5_SE.png",
+        "test": "../bitmaps_test/20190121-101716/nhsuk-frontend_A-Z_navigation_0_document_0_iPhone_5_SE.png",
+        "selector": "document",
+        "fileName": "nhsuk-frontend_A-Z_navigation_0_document_0_iPhone_5_SE.png",
+        "label": "A-Z navigation",
+        "misMatchThreshold": 0.1,
+        "url": "https://en.wikipedia.org/wiki/Main_Page",
+        "expect": 0,
+        "viewportLabel": "iPhone 5/SE",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 389
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/nhsuk-frontend_A-Z_navigation_0_document_1_iPhone_6-8.png",
+        "test": "../bitmaps_test/20190121-101716/nhsuk-frontend_A-Z_navigation_0_document_1_iPhone_6-8.png",
+        "selector": "document",
+        "fileName": "nhsuk-frontend_A-Z_navigation_0_document_1_iPhone_6-8.png",
+        "label": "A-Z navigation",
+        "misMatchThreshold": 0.1,
+        "url": "https://en.wikipedia.org/wiki/Main_Page",
+        "expect": 0,
+        "viewportLabel": "iPhone 6-8",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.02",
+          "analysisTime": 310
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/nhsuk-frontend_A-Z_navigation_0_document_2_iPad.png",
+        "test": "../bitmaps_test/20190121-101716/nhsuk-frontend_A-Z_navigation_0_document_2_iPad.png",
+        "selector": "document",
+        "fileName": "nhsuk-frontend_A-Z_navigation_0_document_2_iPad.png",
+        "label": "A-Z navigation",
+        "misMatchThreshold": 0.1,
+        "url": "https://en.wikipedia.org/wiki/Main_Page",
+        "expect": 0,
+        "viewportLabel": "iPad",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 611
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/nhsuk-frontend_A-Z_navigation_0_document_3_Surface_iPad_Pro.png",
+        "test": "../bitmaps_test/20190121-101716/nhsuk-frontend_A-Z_navigation_0_document_3_Surface_iPad_Pro.png",
+        "selector": "document",
+        "fileName": "nhsuk-frontend_A-Z_navigation_0_document_3_Surface_iPad_Pro.png",
+        "label": "A-Z navigation",
+        "misMatchThreshold": 0.1,
+        "url": "https://en.wikipedia.org/wiki/Main_Page",
+        "expect": 0,
+        "viewportLabel": "Surface, iPad Pro",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 693
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "nhsuk-frontend"
+});
