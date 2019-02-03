4ae7609b43ec1e4488144204a5bb0d7676512db4
surya teja <surya.teja24@yahoo.com>
2019-02-03 15:51:36 +0000

    updated file

--- azure_upload.js
+++ azure_upload.js
@@ -1,8 +1,20 @@
 var azure = require('azure-storage');
 var blobService = azure.createBlobService();
+const STORAGE_ACCOUNT_NAME = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+const ACCOUNT_ACCESS_KEY = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
+const containerName = "backstop";
+const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
+const aborter = Aborter.timeout(30 * ONE_MINUTE);
+const filePath = "tests/";
 
-blobService.createBlockBlobFromLocalFile('backtop', 'backstopimages', 'text.txt', function(error, result, response) {
-    if (!error) {
-        // file uploaded
-    }
-});
+const content = "hello!";
+const fileName = "test.txt";
+
+async function uploadLocalFile(aborter, containerURL, filePath) {
+
+    filePath = path.resolve(filePath);
+
+    const fileName = path.basename(filePath);
+
+    return await uploadFileToBlockBlob(aborter, filePath);
+}
